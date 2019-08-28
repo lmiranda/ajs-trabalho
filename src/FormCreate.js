18285a16847b5c5b350c96e3f19ce1c82f59b54f
Luis Miranda <lmiranda.mao@gmail.com>
2019-08-27 20:57:36 -0400

    Initial commit from Create React App

---
+++ src/FormCreate.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import serializeForm from 'form-serialize'
+//import ImageInput from './utils/ImageInput'
+//import 'bootstrap/dist/css/bootstrap.css'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+ class FormCreate extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = serializeForm(e.target, { hash: true })
+        if (this.props.onAdicionarContato)
+        this.props.onAdicionarContato(values)
+    }
+  render() {
+    return (
+    <container>
+                <Form onSubmit={this.handleSubmit} controlId='FormCreate'>
+                    <Form.Group controlId='group-name' as={Row}>
+                        <Form.Label column sm='2'>Nome</Form.Label>
+                        
+                        <Col sm='10'><Form.Control name='name' placeholder='Nome'/></Col>
+                    </Form.Group>
+                    <Form.Group controlId='group-email' as={Row}>
+                        <Form.Label column sm='2'>Email</Form.Label>
+                        <Col sm='10'><Form.Control name='email' type='email 'placeholder='Email'/></Col>
+                    </Form.Group>
+                    <Button type='submit'>Adicionar</Button>            
+                </Form>
+        
+   </container>
+   )
+ }
+}
+export default FormCreate;
